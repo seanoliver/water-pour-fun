906e6bdaaca5503a8aff8acd8b938cc90ed6b9a6
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2025-02-28 09:16:25 -0800

    Enhance Home page layout with header, footer, and external links for game information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import { GameContainer } from "@/components/GameContainer"
+import Link from "next/link"
 
 export default function Home() {
   return (
@@ -9,10 +10,38 @@ export default function Home() {
         <meta name="description" content="The simple game of pouring water" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Water Pour Fun</h1>
-        <GameContainer />
-      </main>
+      <div className="flex flex-col h-full justify-between">
+        <header className="text-center">
+          <h1 className="text-center text-2xl font-bold">Water</h1>
+          <p className="text-center text-sm">
+            Consolidate colors by pouring water.{" "}
+            <Link
+              href="https://en.wikipedia.org/wiki/Water_pouring_puzzle"
+              target="_blank"
+              className="underline"
+            >
+              Click here
+            </Link>{" "}
+            to learn more.
+          </p>
+        </header>
+        <main className="flex justify-center items-center">
+          <GameContainer />
+        </main>
+        <footer className="text-center text-sm">
+          <p>
+            Made with ❤️ by{" "}
+            <Link
+              href="https://github.com/seanoliver"
+              target="_blank"
+              className="underline"
+            >
+              Sean
+            </Link>{" "}
+            in San Francisco.
+          </p>
+        </footer>
+      </div>
     </div>
   )
 }
